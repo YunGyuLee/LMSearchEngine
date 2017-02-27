040b5fa9913418c4b598d94ce2493855121bcf2a
new_mac <my_mac@new-macui-mbp.connectify>
2017-02-28 02:06:01 +0900

    Initial Commit

---
+++ LMSearchEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LMSearchEngine
+//
+//  Created by new_mac on 2017. 2. 28..
+//  Copyright © 2017년 new_mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
